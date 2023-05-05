aa6f6b874e95b58c15fd84bf94385862ce4239c8
Sviridenko Artem~ <vhavabox@gmail.com>
2023-05-05 13:02:37 +0300

    Task 7 - Task7Test.java - fix

--- src/test/java/ru/raiffeisen/education/Task7Test.java
+++ src/test/java/ru/raiffeisen/education/Task7Test.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static java.time.Duration.ofSeconds;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.openqa.selenium.By.cssSelector;
 import static org.openqa.selenium.By.xpath;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
@@ -33,16 +35,13 @@ public class Task7Test {
 
         driver.navigate().to("http://localhost/litecart/");
 
-        var elementsCount = driver.findElements(cssSelector("nav.content ul.list-vertical li a")).size();
+        var productElements = driver.findElements(cssSelector("li.product"));
 
-        for (var counter = 1; counter <= elementsCount; counter++) {
-            var categoryLinkElement = driver.findElement(cssSelector(String.format("nav.content ul.list-vertical li:nth-child(%s) a", counter)));
-            categoryLinkElement.click();
-            driver.findElements(cssSelector("ul[class='listing-wrapper products'] div.image-wrapper")).forEach(divImageWrapperElement -> {
-                var countOfStickers = divImageWrapperElement.findElements(cssSelector("[class^='sticker']")).size();
-                assertEquals(1, countOfStickers);
-            });
+        for (WebElement productElement : productElements) {
+            var stickerElementsForProduct = productElement.findElements(By.cssSelector(".sticker"));
+            assertEquals(1, stickerElementsForProduct.size());
         }
+
     }
 
     @AfterEach
